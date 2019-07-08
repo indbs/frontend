46ff75a615fb0e382486b8893931ff8161dcbc5b
Elena Avdeeva <eavdeeva@Ferro-Avdeeva.Ferro.local>
2019-07-08 08:47:06 +0300

    menu

---
+++ src/App2.js
@@ -0,0 +1,90 @@
+
+import * as React from "react";
+
+
+
+import {BrowserRouter,Route, Redirect} from "react-router-dom";
+
+
+
+
+import MenuItem from './MenuItem';
+import MenuButton from './MenuButton';
+import Menu from './Menu';
+import Footer from './Footer';
+
+
+class App2 extends React.Component {
+    constructor(props){
+      super(props);
+      this.state={
+        menuOpen:false,
+      }
+    }
+    
+    handleMenuClick() {
+      this.setState({menuOpen:!this.state.menuOpen});
+    }
+    
+    handleLinkClick() {
+      this.setState({menuOpen: false});
+    }
+    
+    render(){
+      const styles= 
+        {
+          container:{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: '99',
+            opacity: 0.9,
+            display:'flex',
+            alignItems:'center',
+            background: 'black',
+            width: '100%',
+            color: 'white',
+            fontFamily:'Lobster',
+          },
+          logo: {
+            margin: '0 auto',
+          },
+          body: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100vw',
+            height: '100vh',
+            filter: this.state.menuOpen ? 'blur(2px)':null,
+            transition: 'filter 0.5s ease',
+          },
+        }
+      const menu = ['About Us','Our Products','Services','FAQ','Contact Us']
+      const menuItems = menu.map((val,index)=>{
+        return (
+          <MenuItem 
+            key={index} 
+            delay={`${index * 0.1}s`}
+            onClick={()=>{this.handleLinkClick();}}>{val}</MenuItem>)
+      });
+      
+      return(
+        <div>
+          <div style={styles.container}>
+            <MenuButton open={this.state.menuOpen} onClick={()=>this.handleMenuClick()} color='white'/>
+            <div style={styles.logo}>Logo</div>
+          </div>
+          <Menu open={this.state.menuOpen}>
+            {menuItems}
+          </Menu>
+          <div style={styles.body}>
+            <Footer name='Menu'/>
+          </div>
+        </div>
+      )
+    }
+  }
+
+ 
+  export default App2;
+ 
---
+++ src/Main.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import App2 from './App2';
+import {BrowserRouter,Route, Redirect} from "react-router-dom";
+
+import './styleMenu.css';
+
+
+var ReactDOMServer = require('react-dom/server');
+require('datejs'); 
+
+
+class Main extends React.Component {
+    render(){
+      const styles = {
+        main: {
+          display:'flex',
+          flexDirection:'column',
+          alignItems: 'center',
+          height: '100vh',
+        }
+      }
+      
+      return (
+
+        
+        <div style={styles.main}>
+          <App2 />
+          <p>  HELOO </p>
+        </div>
+
+      
+      )
+    }
+  }
+  export default Main;
