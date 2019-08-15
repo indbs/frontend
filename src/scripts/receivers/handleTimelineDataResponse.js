abf9859218fd298f15f609cb378c5b4c33b4ee83
Boris <b.smirnov@rusgates.ru>
2019-08-15 16:29:12 +0300

    Ошибка не отображения на gtml текущего обжига.

--- src/scripts/receivers/handleTimelineDataResponse.js
+++ src/scripts/receivers/handleTimelineDataResponse.js
@@ -6,15 +6,14 @@ import {timelineFR06RowBurn, timelineFR06RowStop, timelineFR06RowStart, timeline
 export function handleResponseRaisa (response) {
   const rowsTimeLine=[];
   const dataTimeLine=response.data[1];
-  for (let i = 0; i < dataTimeLine.length; i += 1) {
-    if ( i < dataTimeLine.length-1) {
-      rowsTimeLine.push(
-        timelineRaisaRowBurn(dataTimeLine[i], 'full'),   
-        timelineRaisaRowStop(dataTimeLine[i], 'stop'),  
-        timelineRaisaRowStart(dataTimeLine[i], 'start'),
-        timelineRaisaRowPauseLost(dataTimeLine[i], dataTimeLine[i + 1])  
-      );
-    }
+  console.log('dataTimeLine before pushed ', dataTimeLine);
+  for (let i = 0; i < dataTimeLine.length; i += 1) { 
+    rowsTimeLine.push(
+      timelineRaisaRowBurn(dataTimeLine[i], 'full'),   
+      timelineRaisaRowStop(dataTimeLine[i], 'stop'),  
+      timelineRaisaRowStart(dataTimeLine[i], 'start'),
+    );
+    if (i !== dataTimeLine.length-1) rowsTimeLine.push(timelineRaisaRowPauseLost(dataTimeLine[i], dataTimeLine[i + 1]));
   }
   return rowsTimeLine;
 }
@@ -23,14 +22,12 @@ export function handleResponseRaisa2 (response) {
   const rowsTimeLine=[];
   const dataTimeLine=response.data[1];
   for (let i = 0; i < dataTimeLine.length; i += 1) {
-    if (i < dataTimeLine.length-1) {
-      rowsTimeLine.push(
-        timelineRaisa2RowBurn(dataTimeLine[i], 'fullraisa2'), 
-        timelineRaisa2RowStop(dataTimeLine[i], 'stop'),
-        timelineRaisa2RowStart(dataTimeLine[i], 'start'),
-        timelineRaisa2RowPauseLost(dataTimeLine[i], dataTimeLine[i + 1])   
-      );
-    }
+    rowsTimeLine.push(
+      timelineRaisa2RowBurn(dataTimeLine[i], 'fullraisa2'), 
+      timelineRaisa2RowStop(dataTimeLine[i], 'stop'),
+      timelineRaisa2RowStart(dataTimeLine[i], 'start'),
+    );
+    if (i !== dataTimeLine.length-1) rowsTimeLine.push(timelineRaisa2RowPauseLost(dataTimeLine[i], dataTimeLine[i + 1]));
   }
   return rowsTimeLine;
 }
@@ -39,14 +36,12 @@ export function handleResponseFR05 (response) {
   const rowsTimeLine=[];
   const dataTimeLine=response.data[1];
   for (let i = 0; i < dataTimeLine.length; i += 1) {
-    if (i < dataTimeLine.length-1) {
-      rowsTimeLine.push(
-        timelineFR05RowBurn(dataTimeLine[i], 'fullfr'),
-        timelineFR05RowStop(dataTimeLine[i], 'stop'), 
-        timelineFR05RowStart(dataTimeLine[i], 'start'), 
-        timelineFR05RowPauseLost(dataTimeLine[i], dataTimeLine[i + 1])
-      );
-    }
+    rowsTimeLine.push(
+      timelineFR05RowBurn(dataTimeLine[i], 'fullfr'),
+      timelineFR05RowStop(dataTimeLine[i], 'stop'), 
+      timelineFR05RowStart(dataTimeLine[i], 'start'), 
+    );
+    if (i !== dataTimeLine.length-1) rowsTimeLine.push(timelineFR05RowPauseLost(dataTimeLine[i], dataTimeLine[i + 1]));
   }
   return rowsTimeLine;      
 }
@@ -55,14 +50,12 @@ export function handleResponseFR06 (response) {
   const rowsTimeLine=[];
   const dataTimeLine=response.data;
   for (let i = 0; i < dataTimeLine.length; i += 1) {
-      if (i < dataTimeLine.length-1) {
-        rowsTimeLine.push(
-          timelineFR06RowBurn(dataTimeLine[i], 'fullfr'),
-          timelineFR06RowStop(dataTimeLine[i], 'stop'),
-          timelineFR06RowStart(dataTimeLine[i], 'start'),
-          timelineFR06RowPauseLost(dataTimeLine[i], dataTimeLine[i + 1])
-        );
-      }
-    }
+    rowsTimeLine.push(
+      timelineFR06RowBurn(dataTimeLine[i], 'fullfr'),
+      timelineFR06RowStop(dataTimeLine[i], 'stop'),
+      timelineFR06RowStart(dataTimeLine[i], 'start'),
+    );
+    if (i !== dataTimeLine.length-1) rowsTimeLine.push(timelineFR06RowPauseLost(dataTimeLine[i], dataTimeLine[i + 1]));
+  }
   return rowsTimeLine;
 }
