74bebae84a710ee67f018cedda754f1f4bba48de
Boris <b.smirnov@rusgates.ru>
2019-09-02 18:10:31 +0300

    Перед webpack

--- src/scripts/receivers/requestData.js
+++ src/scripts/receivers/requestData.js
@@ -5,13 +5,13 @@ import {
 import axios                              from 'axios';
 
 export function RequestTimelineData(kiln, data_url, AuthStr){
-  return axios.get(data_url, {headers: {'Authorization': AuthStr.token}}).then(response=>handleTimelineResponseCommon(  kiln, response));
+  return axios.get(data_url, {headers: {'Authorization': AuthStr.token}}).then(response => handleTimelineResponseCommon(  kiln, response));
 }
 
 export function RequestGraphData(kiln, data_url, AuthStr){
-  return axios.get(data_url, {headers: {'Authorization': AuthStr.token}}).then(response=>handleGraphResponseCommon(     kiln, response));
+  return axios.get(data_url, {headers: {'Authorization': AuthStr.token}}).then(response => handleGraphResponseCommon(     kiln, response));
 }
 
 export function RequestTwoTablesData(kiln, HeatOrGasParameter, data_url, AuthStr){
-  return axios.get(data_url, {headers: {'Authorization': AuthStr.token}}).then(response=>handleTwoTablesResponseCommon( kiln, HeatOrGasParameter, response));
+  return axios.get(data_url, {headers: {'Authorization': AuthStr.token}}).then(response => handleTwoTablesResponseCommon( kiln, HeatOrGasParameter, response));
 }
